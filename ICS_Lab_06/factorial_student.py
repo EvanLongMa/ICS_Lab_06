25ed1422f5b72d7c8c000e5dc2cb6f6ddbe73f23
Evan Long Ma <3am@3AMs-MacBook-Pro.local>
2018-03-08 16:18:56 +0800

    OOP Puzzle Solving

---
+++ ICS_Lab_06/factorial_student.py
@@ -0,0 +1,10 @@
+def factorial(num):
+    return
+
+def main():
+    number = int(input("Enter a nonnegative integer: "))
+    fact = factorial(number)
+    print("The factorial of", number, "is", fact)
+        
+main()
+print(12)
---
+++ ICS_Lab_06/number_placement_student.py
@@ -0,0 +1,34 @@
+import random
+NUM_INT = 10
+
+class MinMaxQueue:
+    def __init__(self, l):
+        l.sort()
+        self.sorted_q = l
+
+    def pop_min(self):
+        #perform on the min
+    
+        return self.sorted_q.pop(0)
+
+    def pop_max(self):
+        #perform on the max
+
+        return self.sorted_q.pop()
+
+def main():
+    #create the lists of integers and signs, respectively
+    li = [random.randint(0, 20) for i in range(NUM_INT)]
+    li = list(set(li))
+    sign_array = ["<" if random.randint(0, 1) else ">" for i in range(len(li) - 1)]
+    mmq = MinMaxQueue(li)
+
+    result = []
+    #decide if you'd take out the min or max of the integer list
+    #and append corresponding sign after it
+
+    print(result)
+
+main()
+
+
